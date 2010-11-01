e518689a42fab9a0136a47b34f97aa1ed453d423
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2010-11-01 08:23:21 +0100

    skeleton

---
+++ image_search/apps/image_search/main.js
@@ -0,0 +1,30 @@
+// ==========================================================================
+// Project:   ImageSearch
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals ImageSearch */
+
+// This is the function that will start your app running.  The default
+// implementation will load any fixtures you have created then instantiate
+// your controllers and awake the elements on your page.
+//
+// As you develop your application you will probably want to override this.
+// See comments for some pointers on what to do next.
+//
+ImageSearch.main = function main() {
+
+  // Step 1: Instantiate Your Views
+  // The default code here will make the mainPane for your application visible
+  // on screen.  If you app gets any level of complexity, you will probably 
+  // create multiple pages and panes.  
+  ImageSearch.getPath('mainPage.mainPane').append() ;
+
+  // Step 2. Set the content property on your primary controller.
+  // This will make your app come alive!
+
+  // TODO: Set the content property on your primary controller
+  // ex: ImageSearch.contactsController.set('content',ImageSearch.contacts);
+
+} ;
+
+function main() { ImageSearch.main(); }
