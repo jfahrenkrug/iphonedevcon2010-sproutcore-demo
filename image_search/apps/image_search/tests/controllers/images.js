90af3e8aaee40173184c110c16a8b5b71422bd46
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2010-11-23 15:33:36 +0100

    added a unit test

--- image_search/apps/image_search/tests/controllers/images.js
+++ image_search/apps/image_search/tests/controllers/images.js
@@ -4,12 +4,27 @@
 // ==========================================================================
 /*globals ImageSearch module test ok equals same stop start */
 
-module("ImageSearch.imagesController");
+module("ImageSearch.imagesController", {
+  setup: function() {
+    store = SC.Store.create().from(SC.Record.fixtures);
+  },
+  
+  teardown: function() {
+    store = null;
+  }
+});
 
 // TODO: Replace with real unit test for ImageSearch.imagesController
-test("test description", function() {
-  var expected = "test";
-  var result   = "test";
-  equals(result, expected, "test should equal test");
+test("the summary method", function() {
+  var expected = "No images";
+  var result   = ImageSearch.imagesController.get('summary');
+  equals(result, expected, "summary should equal No images");
+  
+  var query = SC.Query.local(ImageSearch.GoogleImage);
+  var images = store.find(query);
+  ImageSearch.imagesController.set('content', images);
+  expected = "8 images";
+  result   = ImageSearch.imagesController.get('summary');
+  equals(result, expected, "summary should equal 8 images");
 });
 
