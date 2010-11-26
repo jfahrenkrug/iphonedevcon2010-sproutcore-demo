32837f14aa3ac10acefc1cec56758916cb8f2901
Johannes Fahrenkrug <jfahrenkrug@gmail.com>
2010-11-26 12:00:53 +0100

    added loading spinner

--- image_search/apps/image_search/views/resizable_image.js
+++ image_search/apps/image_search/views/resizable_image.js
@@ -14,6 +14,18 @@ ImageSearch.ResizableImageView = SC.ImageView.extend(
 /** @scope ImageSearch.ResizableImageView.prototype */ {
     
     currentImageSize: {width: 0, height: 0},
+        
+    showLoadingSpinner: true,
+    
+    render: function(context, firstTime) {
+      sc_super();
+      
+      
+      if (this.get('status') === SC.IMAGE_STATE_LOADING && this.get('showLoadingSpinner')) {
+        context.attr('src', static_url('resources/spinner.gif'));
+      }
+      
+    },
 
     imageDidLoad: function(url, imageOrError) {
         sc_super();
@@ -42,7 +54,7 @@ ImageSearch.ResizableImageView = SC.ImageView.extend(
         var newWidth = w;
         var newHeight = h;
         
-        //if (w >= f.width || h >= f.height) {
+        if (this.get('status') === SC.IMAGE_STATE_LOADED) {
             if (f.height >= (h * (f.width/w))) {
                 newWidth = f.width;
                 newHeight = h * (f.width/w);
@@ -50,16 +62,18 @@ ImageSearch.ResizableImageView = SC.ImageView.extend(
                 newHeight = f.height;
                 newWidth = w * (f.height/h);
             }              
-        //}
-        
+        } else {
+          newHeight = 35;
+          newWidth = 35;
+        }
+  
         if (this.get('layer')) {
             var imageStyle = this.get('layer').style;
-
+            
             imageStyle.width = newWidth + 'px';
             imageStyle.height = newHeight + 'px';
             imageStyle.top = ((f.height - newHeight) / 2) + 'px';
             imageStyle.left = ((f.width - newWidth) / 2) + 'px';
         }
-    }
-
+    }.observes('status'),
 });
